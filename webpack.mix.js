ecf5eb6e7b27d9f4612dfa5605f2f8624462696f
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-06-17 11:34:04 -0400

    Add vendor code splitting

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.ts('resources/js/app.ts', 'public/js')
+    .extract()
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
     .version();
