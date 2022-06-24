1a1e1c1becbec443a3459286af0b97ddf82fe571
Kelisnor <kelis@LAPTOP-F1F14TG3.localdomain>
2022-06-24 14:18:34 -0400

    delete manully creating of 'created_at' attribute for Assignment migration

--- database/migrations/2022_06_17_181539_create_assignments_table.php
+++ database/migrations/2022_06_17_181539_create_assignments_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('course');
-            $table->timestamp('created_at')->nullable();
             $table->dateTime('due_at')->nullable();
             $table->timestamps();
         });
