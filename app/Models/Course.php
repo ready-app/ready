e28029dc6c83765a7e2b22d70f53a2748b19c84a
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-07-01 14:14:16 -0400

    Update migrations and entities

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,17 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
-{
+class Course extends Model {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'instructor'
+        'name'
     ];
 
-    public function assignments()
-    {
+    public function assignments() {
         return $this->hasMany(Assignment::class);
     }
 
--- database/migrations/2022_06_17_181143_create_courses_table.php
+++ database/migrations/2022_06_17_181143_create_courses_table.php
@@ -4,19 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('instructor');
             $table->timestamps();
         });
     }
@@ -26,8 +23,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('courses');
     }
 };
