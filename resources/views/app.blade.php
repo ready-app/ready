1a6cbb9a539367633b204ec383df6f0deefb94e3
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-07-22 15:30:40 -0400

    Switch to official laravel vite plugin (#86)
    * Switch to official laravel vite plugin
    
    * Lock deps
    
    * Remove commented code
    
    * Add vite port for docker
    
    * Fix HTTPS issue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <title>READY</title>
-        @vite
+        @vite(["resources/js/app.ts", "resources/sass/app.scss"])
         @inertiaHead
     </head>
     <body>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
 import { defineConfig } from "vite";
-import laravel from "vite-plugin-laravel";
+import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import * as path from "path";
+import * as os from "os";
+import { config } from "dotenv";
+
+config();
+
+const url = new URL(process.env.APP_URL as string);
+
+const localConfig: { host: string, https: boolean | object } = {
+    host: url.host,
+    https: false
+};
+
+// for Laravel Valet
+if (localConfig.host !== "localhost" && process.env.VITE_APP_ENV === "dev") {
+    localConfig.https = {
+        key: os.homedir() + "/.config/valet/Certificates/" + url.host + ".key",
+        cert: os.homedir() + "/.config/valet/Certificates/" + url.host + ".crt"
+    };
+}
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0"
-    },
+    server: localConfig,
     plugins: [
-        laravel(),
+        laravel(["resources/js/app.ts", "resources/sass/app.scss"]),
         vue()
-    ]
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js")
+        }
+    }
 });
