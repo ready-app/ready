55fa13b3fbdfb314cc734cea46d0a9d98c6edc89
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-07-27 12:40:03 -0400

    Update CI

--- config/database.php
+++ config/database.php
@@ -91,12 +91,6 @@ return [
             'prefix_indexes' => true
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-
-        'testing' => [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => ''
         ]
 
     ],
