4e05200fc340608b1c12da55f357978821aee2be
Kelisnor <kelis@LAPTOP-F1F14TG3.localdomain>
2022-08-02 14:39:13 -0400

    rewrite functions with is_admin attribute

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -16,53 +16,38 @@ class AssignmentController extends Controller
         return inertia("AdminPage");
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create(User $user): RedirectResponse {
-        if ($user->dreate()) {
-            return redirect()->route('admin.index')->with('success', 'User deleted');
+        if ($user->is_admin) {
+            return redirect()->route('admin.index')->with('success', 'Assignment created');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to delete user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 
 
     public function findAssignmentByCourse($course_id)
     {
-        $course_id = Assignment::where('course_id', $course_id)->get();
-        foreach ($users as $user) {
-            $user->type_name=$user->booktype->type_name;
+        $assignments_list = Assignment::where('course_id', $course_id)->get();
+        foreach ($assignment as $assignments_list) {
+            $assignment->name=$assignments_list->assignment;
         }
-        return response()->json(['status'=>1,'msg'=>'S','data'=>$users]);
+        return response()->json(['data'=>$assignment]);
     }
 
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UserUpdateRequest $request, User $user): RedirectResponse {
-        if ($user->update($request->validated())) {
-            return redirect()->route('admin.index')->with('success', 'User updated');
+
+    public function update(User $user): RedirectResponse {
+        if ($user->is_admin) {
+            return redirect()->route('admin.index')->with('success', 'Assignment created');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to update user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy(User $user): RedirectResponse {
         if ($user->delete()) {
-            return redirect()->route('admin.index')->with('success', 'User deleted');
+            return redirect()->route('admin.index')->with('success', 'Assignment created');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to delete user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 }
--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -16,44 +16,27 @@ class CourseController extends Controller
         return inertia("AdminPage");
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create(User $user): RedirectResponse {
-        if ($user->dreate()) {
-            return redirect()->route('admin.index')->with('success', 'User deleted');
+        if ($user->is_admin) {
+            return redirect()->route('admin.index')->with('success', 'Assignment created');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to delete user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 
 
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UserUpdateRequest $request, User $user): RedirectResponse {
-        if ($user->update($request->validated())) {
-            return redirect()->route('admin.index')->with('success', 'User updated');
+    public function update(User $user): RedirectResponse {
+        if ($user->is_admin) {
+            return redirect()->route('admin.index')->with('success', 'Assignment updated');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to update user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy(User $user): RedirectResponse {
         if ($user->delete()) {
-            return redirect()->route('admin.index')->with('success', 'User deleted');
+            return redirect()->route('admin.index')->with('success', 'Assignment deleted');
         }
-        return redirect()->route('admin.index')->with('error', 'Failed to delete user');
+        return redirect()->route('admin.index')->with('error', 'User is not an admin');
     }
 }
