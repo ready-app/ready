a56539b136a9f5fda3d4225114f1f8113213cfad
Kelisnor <kelis@LAPTOP-F1F14TG3.localdomain>
2022-08-02 14:09:59 -0400

    delete unused functions

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -31,38 +31,6 @@ class CourseController extends Controller
 
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request){
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
