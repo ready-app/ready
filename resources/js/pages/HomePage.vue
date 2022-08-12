eaa9d43d11686acea804f5c51a5e67a15eb0223a
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-08-12 14:19:49 -0400

    Add homepage and update bootstrap

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,32 +1,42 @@
 <template>
-    <p>{{ counter }}</p>
-    <button
-        @click="increment"
-        class="btn btn-secondary"
-    >
-        Increment
-    </button><br>
-    <TheModal
-        title="Cool Modal"
-        button-text="Open Modal"
-        button-classes="btn btn-primary"
-    >
-        <p>modal content</p>
-    </TheModal>
-    <Link href="/login">Login page</Link><br>
-    <Link href="/schedule">Go to schedule</Link>
+    <div class="p-5 mb-4 bg-light rounded-3 mt-4">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">READY</h1>
+            <p class="fs-4">A web app to help manage your coursework!</p>
+            <Link
+                class="btn btn-primary"
+                href="/register"
+            >
+                Sign up now!
+            </Link>
+        </div>
+    </div>
+    <div class="row align-items-md-stretch">
+        <div class="col">
+            <div class="h-100 p-5 bg-light border rounded-3">
+                <h2>Weekly Schedule</h2>
+                <p>View a weekly schedule of your classes, assignments, exams, and more!</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="h-100 p-5 bg-light border rounded-3">
+                <h2>Assignment Tracker</h2>
+                <p>Keeps track of your assignments, exams, and more!</p>
+            </div>
+        </div>
+        <div class="col">
+            <div class="h-100 p-5 bg-light border rounded-3">
+                <h2>Easily add classes</h2>
+                <p>Provide a list of your CRNs and we'll automatically load your classes!</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import { Link } from "@inertiajs/inertia-vue3";
-import TheModal from "@/components/TheModal.vue";
 
-let counter = ref(0);
 
-const increment = () => {
-    counter.value++;
-};
 </script>
 
 <style scoped>
