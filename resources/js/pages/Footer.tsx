f3242becf4ed90d0d2d6ee396f2d7367f59b2fdb
Chris Reed <55092742+cjreed121@users.noreply.github.com>
2022-12-15 02:48:42 -0500

    Revert react switch

--- resources/js/pages/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <div className="text-center">
-            <p className="m-0">READY | <a href="https://github.com/rpi-ready/ready">GitHub</a> | An <a href="https://rcos.io">RCOS</a> Project</p>
-        </div>
-    );
-}
